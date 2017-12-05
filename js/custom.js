cc7c3f9fb0a57fe5df7c1c292ec43574cf4fefb7
Jeremy McWilliams <jeremym@lclark.edu>
2017-12-05 09:47:07 -0800

    add base files for package

---
+++ js/custom.js
@@ -0,0 +1,41 @@
+(function () {
+    "use strict";
+    'use strict';
+
+
+    var app = angular.module('viewCustom', ['angularLoad']);
+
+    /****************************************************************************************************/
+
+        /*In case of CENTRAL_PACKAGE - comment out the below line to replace the other module definition*/
+
+        /*var app = angular.module('centralCustom', ['angularLoad']);*/
+
+    /****************************************************************************************************/
+
+
+})
+
+console.log("from customization files");function loadScript(url, callback)
+{
+    // Adding the script tag to the head as suggested before
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+
+    // Then bind the event to the callback function.
+    // There are several events for cross browser compatibility.
+    script.onreadystatechange = callback;
+    script.onload = callback;
+
+    // Fire the loading
+    head.appendChild(script);
+}
+
+var scripts_loaded = function() {
+   console.log("external scripts loaded");
+};
+
+loadScript("//library.lclark.edu/src/stag/watzek_js.js", scripts_loaded);
+   
